00534c9b7c5a1325652547fb0c890113c0f5e85d
Robert Perper <rperper@litespeedtech.com>
2021-04-13 16:45:08 -0400

    Version 1.4.20

--- src/memcache/lsmemcache.h
+++ src/memcache/lsmemcache.h
@@ -29,7 +29,7 @@
 #include <util/gpointerlist.h>
 #include <util/objpool.h>
 
-#define VERSION_TO_LOG  "1.4.19"
+#define VERSION_TO_LOG  "1.4.20"
 #define VERSION         "1.0.0"
 
 #define ULL_MAXLEN      24      // maximum buffer size of unsigned long long
